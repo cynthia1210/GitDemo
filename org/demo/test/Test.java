e2352848fc1ebc36db6a92e952a2e2eb5fd2d90f
cynthia1210 <cynthiachaudhary1@gmail.com>
2023-10-21 17:32:38 +0530

    Add

--- org/demo/test/Test.java
+++ org/demo/test/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, This is a demo class ");
-		System.out.println("Hi, This is Vinay Branch");
 		System.out.println("Hi, this is sujay Branch");
 				
 			}
