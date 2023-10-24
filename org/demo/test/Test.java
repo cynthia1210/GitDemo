520ecdf1bf456ffff1f960732b7def380b7bd2f3
cynthia1210 <cynthiachaudhary1@gmail.com>
2023-10-24 11:32:21 +0530

    practice

--- org/demo/test/Test.java
+++ org/demo/test/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, This is a demo class ");
-		System.out.println("Hi, this is Vinay Branch");	
+		System.out.println("This is a github practice class");
 			}
 }
